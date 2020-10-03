7e9489c1083711774eea4772453e6091bab995cf
Joshua Kern <joshuakerndev@gmail.com>
2020-10-03 15:26:20 -0700

    attempt to resolve safari issues part 1

--- src/Flashcard/Flashcard.jsx
+++ src/Flashcard/Flashcard.jsx
@@ -9,16 +9,18 @@ const Flashcard = ({ card, isQuiz }) => {
     const handleClick = () => setIsRotated(!isRotated);
 
     return(
-        <div 
-            className='Flashcard' 
-            id={isRotated ? "isRotated" : null}
-            onClick={handleClick}
-        >
-            <div className='Card-Front'>
-                <p className='Flashcard-Text'>{card.English}</p>
-            </div>
-            <div className='Card-Back'>
-                <p className='Flashcard-Text'>{isQuiz ? card.English : card.Chinese}</p>
+        <div className="FlashcardContainer">
+            <div 
+                className='Flashcard' 
+                id={isRotated ? "isRotated" : null}
+                onClick={handleClick}
+            >
+                <div className='Card-Front'>
+                    <p className='Flashcard-Text'>{card.English}</p>
+                </div>
+                <div className='Card-Back'>
+                    <p className='Flashcard-Text'>{isQuiz ? card.English : card.Chinese}</p>
+                </div>
             </div>
         </div>
     );
