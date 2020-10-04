6a4a0419743228922b429e87a57e7be018b7a4ef
Joshua Kern <joshuakerndev@gmail.com>
2020-10-04 13:59:47 -0700

    add card-flipping component

--- src/Flashcard/Flashcard.jsx
+++ src/Flashcard/Flashcard.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import './Flashcard.css';
 
+import ReactCardFlip from 'react-card-flip';
+
+import { Button } from 'reactstrap';
 
 const Flashcard = ({ card, isQuiz }) => {
 
@@ -8,19 +11,36 @@ const Flashcard = ({ card, isQuiz }) => {
 
     const handleClick = () => setIsRotated(!isRotated);
 
+    const [isFlipped, setIsFlipped] = useState(false);
+
+    const handleFlip = () => setIsFlipped(!isFlipped);
+
     return(
-        <div 
-            className='Flashcard' 
-            id={isRotated ? "isRotated" : null}
-            onClick={handleClick}
+        // <div 
+        //     className='Flashcard' 
+        //     id={isRotated ? "isRotated" : null}
+        //     onClick={handleClick}
+        // >
+        //     <div className='Card-Front'>
+        //         <p className='Flashcard-Text'>{card.English}</p>
+        //     </div>
+        //     <div className='Card-Back'>
+        //         <p className='Flashcard-Text'>{isQuiz ? card.English : card.Chinese}</p>
+        //     </div>
+        // </div>
+        <ReactCardFlip 
+            isFlipped={isFlipped} 
+            flipDirection="horizontal"
+            className="Flashcard"
         >
-            <div className='Card-Front'>
-                <p className='Flashcard-Text'>{card.English}</p>
+            <div className="Card-Front" onClick={handleFlip} >
+                <p className="Flashcard-Text">{card.English}</p>
             </div>
-            <div className='Card-Back'>
-                <p className='Flashcard-Text'>{isQuiz ? card.English : card.Chinese}</p>
+    
+            <div className="Card-Back" onClick={handleFlip}>
+                <p className="Flashcard-Text">{isQuiz ? card.English : card.Chinese}</p>
             </div>
-        </div>
+        </ReactCardFlip>
     );
 }
 
