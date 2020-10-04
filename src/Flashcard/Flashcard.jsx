a2af2653d8b9445ecfe4692883f43ea66df1f243
Joshua Kern <joshuakerndev@gmail.com>
2020-10-04 14:07:09 -0700

    clean up Flashcard jsx and css markup

--- src/Flashcard/Flashcard.jsx
+++ src/Flashcard/Flashcard.jsx
@@ -3,31 +3,12 @@ import './Flashcard.css';
 
 import ReactCardFlip from 'react-card-flip';
 
-import { Button } from 'reactstrap';
-
 const Flashcard = ({ card, isQuiz }) => {
 
-    const [isRotated, setIsRotated] = useState(false);
-
-    const handleClick = () => setIsRotated(!isRotated);
-
     const [isFlipped, setIsFlipped] = useState(false);
-
     const handleFlip = () => setIsFlipped(!isFlipped);
 
     return(
-        // <div 
-        //     className='Flashcard' 
-        //     id={isRotated ? "isRotated" : null}
-        //     onClick={handleClick}
-        // >
-        //     <div className='Card-Front'>
-        //         <p className='Flashcard-Text'>{card.English}</p>
-        //     </div>
-        //     <div className='Card-Back'>
-        //         <p className='Flashcard-Text'>{isQuiz ? card.English : card.Chinese}</p>
-        //     </div>
-        // </div>
         <ReactCardFlip 
             isFlipped={isFlipped} 
             flipDirection="horizontal"
