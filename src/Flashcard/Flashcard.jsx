1cf186866c23d23bbf23a70b3a9163a2624f2ff9
Joshua Kern <joshuakerndev@gmail.com>
2020-10-03 18:08:44 -0700

    test purple cards on safari

--- src/Flashcard/Flashcard.jsx
+++ src/Flashcard/Flashcard.jsx
@@ -8,18 +8,30 @@ const Flashcard = ({ card, isQuiz }) => {
 
     const handleClick = () => setIsRotated(!isRotated);
 
+    const [isFlipped, setIsFlipped] = useState(false);
+
+    const handleFlip = () => setIsFlipped(!isFlipped);
+
     return(
-        <div 
-            className='Flashcard' 
-            id={isRotated ? "isRotated" : null}
-            onClick={handleClick}
-        >
-            <div className='Card-Front'>
-                <p className='Flashcard-Text'>{card.English}</p>
+        <div>
+            <div 
+                className='Flashcard' 
+                id={isRotated ? "isRotated" : null}
+                onClick={handleClick}
+            >
+                <div className='Card-Front'>
+                    <p className='Flashcard-Text'>{card.English}</p>
+                </div>
+                <div className='Card-Back'>
+                    <p className='Flashcard-Text'>{isQuiz ? card.English : card.Chinese}</p>
+                </div>
             </div>
-            <div className='Card-Back'>
-                <p className='Flashcard-Text'>{isQuiz ? card.English : card.Chinese}</p>
-            </div>
+            <div className="flipCard" onClick={handleFlip}> 
+            <div className="card" id={isFlipped ? "flipped" : ""}> 
+                <div className="side front">Keyword</div> 
+                <div className="side back">Definition<br/>Explanation</div> 
+            </div> 
+            </div> 
         </div>
     );
 }
