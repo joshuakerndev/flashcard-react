3ed6a67dabd207c62c89d2b403b5d3515b6782e8
Joshua Kern <joshuakerndev@gmail.com>
2020-10-03 18:19:26 -0700

    revert back to earlier version

--- src/Flashcard/Flashcard.jsx
+++ src/Flashcard/Flashcard.jsx
@@ -8,30 +8,18 @@ const Flashcard = ({ card, isQuiz }) => {
 
     const handleClick = () => setIsRotated(!isRotated);
 
-    const [isFlipped, setIsFlipped] = useState(false);
-
-    const handleFlip = () => setIsFlipped(!isFlipped);
-
     return(
-        <div>
-            <div 
-                className='Flashcard' 
-                id={isRotated ? "isRotated" : null}
-                onClick={handleClick}
-            >
-                <div className='Card-Front'>
-                    <p className='Flashcard-Text'>{card.English}</p>
-                </div>
-                <div className='Card-Back'>
-                    <p className='Flashcard-Text'>{isQuiz ? card.English : card.Chinese}</p>
-                </div>
+        <div 
+            className='Flashcard' 
+            id={isRotated ? "isRotated" : null}
+            onClick={handleClick}
+        >
+            <div className='Card-Front'>
+                <p className='Flashcard-Text'>{card.English}</p>
             </div>
-            <div className="flipCard" onClick={handleFlip}> 
-            <div className="card" id={isFlipped ? "flipped" : ""}> 
-                <div className="side front">Keyword</div> 
-                <div className="side back">Definition<br/>Explanation</div> 
-            </div> 
-            </div> 
+            <div className='Card-Back'>
+                <p className='Flashcard-Text'>{isQuiz ? card.English : card.Chinese}</p>
+            </div>
         </div>
     );
 }
